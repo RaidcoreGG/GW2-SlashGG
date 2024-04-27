c32e200c6257c2df9b79d99072805aa68cd30b22
Delta <46466697+DeltaGW2@users.noreply.github.com>
2024-04-27 17:20:22 +0200

    fix script files

--- src/Remote.h
+++
@@ -1,2 +0,0 @@
-#pragma once 
-#define REMOTE_URL "https://github.com/RaidcoreGG/GW2-SlashGG" 
--- src/Version.h
+++
@@ -1,5 +0,0 @@
-#pragma once 
-#define V_MAJOR 2024 
-#define V_MINOR 4 
-#define V_BUILD 27 
-#define V_REVISION 1026 
